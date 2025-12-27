60b53d4cb86c99fe021fb5260dbab733c767282f
Antonia <anto.salinas.a@gmail.com>
2025-12-27 16:53:41 -0300

    Segundo ejercicio incompleto

---
+++ Ejercicio 2.py
@@ -0,0 +1,11 @@
+palabra1 = input()
+palabra2 = input()
+i = 0;
+
+if len(palabra1) == len(palabra2):
+    for i in range(len(palabra1)):
+        x = palabra1[i]
+        y = 0;
+        if x == palabra2[]
+else:
+    print("False")
