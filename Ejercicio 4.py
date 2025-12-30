2ce1019778cf02e0138c9908f436b22f9029834f
Antonia <anto.salinas.a@gmail.com>
2025-12-30 13:17:37 -0300

    Me costó tmb

--- Ejercicio 4.py
+++ Ejercicio 4.py
@@ -1,8 +1,17 @@
 #Es primo
 
-n = 1
+def es_primo(num):
+    if num <= 2:
+        return True
+    for n in range(2, num):
+        if num % n == 0:
+            return False
+    return True
 
 for i in range(1, 101):
-    if n%i == 1:
-        print(n)
-    n += 1
+    if es_primo(i):
+        print(i, " -> Es primo")
+    else:
+        print(i, "-> No es primo")
+    
+    
