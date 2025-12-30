1da99ce1ffd31c7a3adf661b7baf57d12c8ab547
Antonia <anto.salinas.a@gmail.com>
2025-12-30 13:49:13 -0300

    Listoco

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -10,4 +10,4 @@ for i in range(1,101):
     else:
         print(i)
 
-    
+
--- Ejercicio 5.py
+++ Ejercicio 5.py
@@ -1,9 +1,13 @@
 #Área de un polígono
 
-p = print("Ingrese el polígono: "), input()
-a = print("Ingrese la altura: "), input()
-l = print("Ingrese el largo: "), input()
-
+p = input("Ingrese el polígono: ")
 p.lower()
 
-if p != "triangulo"
+if p != "triangulo":
+    a = int(input("Ingrese la altura: "))
+    l = int(input("Ingrese el largo: "))
+    print("El área es: ", a*l)
+else:
+    b = int(input("Ingrese la base: "))
+    h = int(input("Ingrese la altura: "))
+    print("Él area es: ", (b*h)/2)
